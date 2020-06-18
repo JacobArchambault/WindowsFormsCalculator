0a6d50420931769bb26d659863b558e55e2f85df
Jacob Archambault <jacob.archambault@waystar.com>
2020-06-17 23:42:31 -0400

    Add textboxes and buttons with Add and Multiply text.

--- Calculator.cs
+++ Calculator.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsCalculator
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        public Calculator()
         {
             InitializeComponent();
         }
 
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
