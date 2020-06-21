a92ec6d330e6b6352844eae0d2e7f0499845aa8e
Jacob Archambault <jacob.archambault@waystar.com>
2020-06-20 21:40:31 -0400

    Achieve working project

--- Calculator.Designer.cs
+++ Calculator.Designer.cs
@@ -28,53 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.value1Textbox = new System.Windows.Forms.TextBox();
+            this.value2Textbox = new System.Windows.Forms.TextBox();
+            this.value1Label = new System.Windows.Forms.Label();
+            this.value2Label = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // addButton
             // 
-            this.button1.Location = new System.Drawing.Point(126, 86);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.addButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.addButton.Location = new System.Drawing.Point(191, 118);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(112, 34);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // multiplyButton
             // 
-            this.button2.Location = new System.Drawing.Point(126, 126);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 34);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Multiply";
-            this.button2.UseVisualStyleBackColor = true;
+            this.multiplyButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.multiplyButton.Location = new System.Drawing.Point(191, 158);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(112, 34);
+            this.multiplyButton.TabIndex = 1;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = false;
+            this.multiplyButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // value1Textbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(88, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 31);
-            this.textBox1.TabIndex = 2;
+            this.value1Textbox.Location = new System.Drawing.Point(191, 44);
+            this.value1Textbox.Name = "value1Textbox";
+            this.value1Textbox.Size = new System.Drawing.Size(112, 31);
+            this.value1Textbox.TabIndex = 2;
+            this.value1Textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // textBox2
+            // value2Textbox
             // 
-            this.textBox2.Location = new System.Drawing.Point(88, 49);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 31);
-            this.textBox2.TabIndex = 3;
+            this.value2Textbox.Location = new System.Drawing.Point(191, 81);
+            this.value2Textbox.Name = "value2Textbox";
+            this.value2Textbox.Size = new System.Drawing.Size(112, 31);
+            this.value2Textbox.TabIndex = 3;
+            this.value2Textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // value1Label
+            // 
+            this.value1Label.AutoSize = true;
+            this.value1Label.Location = new System.Drawing.Point(12, 44);
+            this.value1Label.Name = "value1Label";
+            this.value1Label.Size = new System.Drawing.Size(137, 25);
+            this.value1Label.TabIndex = 4;
+            this.value1Label.Text = "Enter first value:";
+            // 
+            // value2Label
+            // 
+            this.value2Label.AutoSize = true;
+            this.value2Label.Location = new System.Drawing.Point(12, 81);
+            this.value2Label.Name = "value2Label";
+            this.value2Label.Size = new System.Drawing.Size(164, 25);
+            this.value2Label.TabIndex = 5;
+            this.value2Label.Text = "Enter second value:";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(55, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(200, 32);
+            this.title.TabIndex = 6;
+            this.title.Text = "Simple Calculator";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.resultLabel.Location = new System.Drawing.Point(44, 195);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(259, 25);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "Value must be numeric and > 0";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.resultLabel.Visible = false;
             // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 203);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(315, 286);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.value2Label);
+            this.Controls.Add(this.value1Label);
+            this.Controls.Add(this.value2Textbox);
+            this.Controls.Add(this.value1Textbox);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.addButton);
+            this.ForeColor = System.Drawing.Color.Yellow;
             this.Name = "Calculator";
             this.Text = "Calculator";
             this.Load += new System.EventHandler(this.Calculator_Load);
@@ -85,9 +141,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label length;
+        private System.Windows.Forms.Label width;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Label value1Label;
+        private System.Windows.Forms.Label value2Label;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TextBox value1Textbox;
+        private System.Windows.Forms.TextBox value2Textbox;
     }
 }
--- Calculator.cs
+++ Calculator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq.Expressions;
 using System.Text;
 using System.Windows.Forms;
 
@@ -19,5 +20,51 @@ namespace WindowsFormsCalculator
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double input1 = GetDouble(textBox1);
+                double input2 = GetDouble(textBox2);
+                resultLabel.Text = (input1 + input2).ToString();
+                resultLabel.ForeColor = Color.Yellow;
+                resultLabel.Visible = true;
+            }
+            catch
+            {
+                resultLabel.Text = "Value must be numeric and > 0.";
+                resultLabel.ForeColor = Color.Red;
+                resultLabel.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double input1 = GetDouble(textBox1);
+                double input2 = GetDouble(textBox2);
+                resultLabel.Text = (input1 * input2).ToString();
+                resultLabel.ForeColor = Color.Yellow;
+                resultLabel.Visible = true;
+            }
+            catch
+            {
+                resultLabel.Text = "Value must be numeric and > 0.";
+                resultLabel.ForeColor = Color.Red;
+                resultLabel.Visible = true;
+            }
+
+        }
+
+        private double GetDouble(TextBox fromTextBox)
+        {
+            double myDouble = 0;
+            // Ensure user enters a number
+            myDouble = double.Parse(fromTextBox.Text);
+            return myDouble;
+        }
+
     }
 }
